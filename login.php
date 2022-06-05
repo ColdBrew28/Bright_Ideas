99f4cb068916bb6b0cb51e6cb4657cce5b43f3c0
cassyisaguirre <82536776+cassyisaguirre@users.noreply.github.com>
2022-06-06 00:56:00 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,30 +1,52 @@
 <?php
-$host =  "localhost";
-$user =  "root";
-$password = "";
-$db = "theatticlibrary";
+include 'config.php';
+session_start();
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'bright_ideas';
 
-$mysql = new mysqli("username", "email", "password");
-// mysqli_connect($username, $email, $password);
-mysql_select_db($db);
+// Create connection
+$connection = new mysqli($servername, $username, $password, $database);
 
-if(isset($_POST['username'])) {
-    $uname=$_POST['username'];
-    $email=$_POST['email'];
-    $password=$_POST['password'];
 
-    $sql="select * from login where user = '".$uname."'AND PASS'".$password."' limit 1";
+$Username = "";
+$Email_Address = "";
+$Password = "";
 
-    $result=mysql_query($sql);
+$errorMessage = "";
+$successMessage = "";
 
-    if(mysql_num_rows($result)==1) {
-        echo "You have successfully logged in";
-        exit();
-    }
-    else {
-        echo "You have entered incorrect password";
-        exit();
-    }
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $Username = $_POST["Username"];
+    $Email_Address = $_POST["Email_Address"];
+    $Password = $_POST["Password"];
+
+    do {
+        // if empty($id_num) || empty($Name) || empty($Payable_Fee) || empty($Balance)) || empty($Username)) {
+        //     $errorMessage = "All the fields are required";
+        //     break;
+        // }
+
+        // add new client to database
+        $sql =  "INSERT INTO users (Username, Email_Address, Password) " .
+            "VALUES ('$Username', '$Email_Address', '$Password')";
+        $result = $connection->query($sql);
+
+        if (!$result) {
+            $errorMessage = "Invalid query: " . $connection->error;
+            break;
+        }
+
+        $Username = "";
+        $Email_Address = "";
+        $Password = "";        
+
+        $successMessage = "Client added correctly";
+
+        header("location: login.php");
+        exit;
+    } while (false);
 }
 ?>
 
@@ -45,17 +67,19 @@ if(isset($_POST['username'])) {
     <div class="container">
       <div class="forms-container">
         <div class="signin-signup">
-          <form action="#" class="sign-in-form">
+          <form name="signin" action="login_connection.php" class="sign-in-form" method="POST">
             <h2 class="title">Sign in</h2>
             <div class="input-field">
               <i class="fas fa-user"></i>
-              <input type="text" name="username" placeholder="Username" />
+              <input type="text" placeholder="Username" name="Username" required>
             </div>
             <div class="input-field">
               <i class="fas fa-lock"></i>
-              <input type="password" name="password" placeholder="Password" />
+                <input type="password" placeholder="Password" name="Password" required>
             </div>
-            <input type="submit" value="Login" class="btn solid" />
+            
+            <input type="submit" name="submit" value="Login" class="btn solid" />
+            <div class="link"> <a href="index.html"></a></div>
             <p class="social-text">Or Sign in with social platforms</p>
             <div class="social-media">
               <a href="#" class="social-icon">
@@ -72,21 +96,40 @@ if(isset($_POST['username'])) {
               </a>
             </div>
           </form>
-          <form action="#" method="POST" class="sign-up-form">
+
+          
+          <form name="signup" action="" class="sign-up-form" method="post">
             <h2 class="title">Sign up</h2>
             <div class="input-field">
               <i class="fas fa-user"></i>
-              <input type="text" name="username" placeholder="Username" />
+              <input type="text" placeholder="Username" name="Username" value="<?php echo $Username; ?>" >
             </div>
             <div class="input-field">
               <i class="fas fa-envelope"></i>
-              <input type="email" name="email" placeholder="Email" />
+              <input type="text" placeholder="Enter your Email_Address" name="Email_Address" value="<?php echo $Email_Address; ?>" >
             </div>
             <div class="input-field">
               <i class="fas fa-lock"></i>
-              <input type="password" name="password" placeholder="Password" />
+              <input type="password" placeholder="Enter Password" name="Password" value="<?php echo $Password; ?>" >
             </div>
+            
+            <?php
+            if (!empty($successMessage)) {
+                echo "
+                <div class='row mb-3'>
+                    <div class='offset-sm-3 col-sm-6'>
+                        <div class='alert alert-success alert-dismissible fade show' role='alert'>
+                            <strong>$successMessage</strong>
+                            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                        </div>
+                    </div>
+                </div>
+                ";
+            }
+            ?>
+
             <input type="submit" class="btn" value="Sign up" />
+            <div class="link"> <a href="index.html"></a></div>
             <p class="social-text">Or Sign up with social platforms</p>
             <div class="social-media">
               <a href="#" class="social-icon">
